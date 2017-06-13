91e8f5980599d9ff255cb2b22caddf69f467679a
Federico Lo Grasso <lograssofede@gmail.com>
2017-06-13 13:07:20 -0300

    Initial commit

---
+++ Actualizar_Stock_Packs/EditorTxt.cs
@@ -0,0 +1,241 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: SoporteSEM
+ * Date: 13/06/2017
+ * Time: 9:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace Actualizar_Stock_Packs
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class EditorTxt{
+		
+		List<Pack> listaPacks;
+		List<string> listaProductos;
+		public Logger logger = new Logger();
+		const int RESTA_PACKS = 6;//Valor de margen para restar a la cant de packs
+		
+		public struct Pack
+		{
+			public long sku_lamp;
+			public int sku_pack;
+			public int cant_pack;
+			public int stock_pack;
+			
+			public Pack (long sku_lamp, int sku_pack, int cant_pack)
+			{
+				this.sku_lamp=sku_lamp;
+				this.sku_pack=sku_pack;
+				this.cant_pack=cant_pack;
+				this.stock_pack=0;
+			}
+			
+			public override string ToString()
+			{
+				return string.Format("[Pack Sku_lamp={0}, Sku_pack={1}, Cant_pack={2}, Stock_pack={3}]", sku_lamp, sku_pack, cant_pack, stock_pack);
+			}
+
+			
+		}
+		
+		
+		public EditorTxt()
+		{
+		}
+		
+		public void leerPacks(string rutaArchivo)
+		{
+			listaPacks  = new List<Pack>();
+			System.IO.StreamReader reader = new System.IO.StreamReader(@rutaArchivo);
+			string line;
+			
+			while ((line = reader.ReadLine()) != null)
+			{
+				string[] words = line.Split(';');
+				listaPacks.Add(new Pack(Int64.Parse(words[0]),Int32.Parse(words[2]),Int32.Parse(words[1])));
+
+			}
+			reader.Close();
+		}
+		
+		public void chequearExistenciaSKU(string rutaArchivoStocks)
+		{
+			System.IO.StreamReader reader = new System.IO.StreamReader(@rutaArchivoStocks);
+			string text;
+			bool ok = true;
+			text = reader.ReadToEnd();
+			foreach (Pack pack in listaPacks)
+			{
+				if (!text.Contains(pack.sku_lamp.ToString()))
+				{
+					logger.logData("SKU de lampara inexistente: "+pack.sku_lamp);
+					ok=false;
+				}
+				
+				if (!text.Contains(pack.sku_pack.ToString()))
+				{
+					logger.logData("SKU de pack inexistente: "+pack.sku_pack);
+					ok=false;
+				}
+				
+			}
+			if (!ok)
+				sendErrorEmail();
+		}
+		
+		public void leerArchivoStock(string rutaArchivoStocks)
+		{
+			StreamReader reader = new System.IO.StreamReader(@rutaArchivoStocks);
+			string line;
+			listaProductos = new List<string>();
+			
+			while ((line = reader.ReadLine()) != null)
+			{
+				listaProductos.Add(line); //Agrego todas las lineas a una lista
+				
+			}
+			reader.Close();
+			
+		}
+		
+		public void grabarStocks(string rutaArchivoStocks)
+		{
+
+			StreamWriter writer = new StreamWriter(@"MERnuevo.txt");
+			//Crea el nuevo archivo en el disco C
+			StringBuilder aux = new StringBuilder();
+			bool esPack = false;
+			
+			foreach (string texto in listaProductos)
+			{
+				esPack = false;	
+				foreach (Pack pack in listaPacks)
+				{
+					
+					//Tomo solo los primeros 4 caracteres del sku del pack de lamps
+					if (texto.Substring(0,4).Contains(pack.sku_pack.ToString()))
+					{
+						esPack=true;
+						aux.Append(texto); //Cargo la linea al StringBuilder
+						aux.Remove(95,10); //Borro todo dsp del precio
+						if(pack.stock_pack.ToString().Length == 1)
+						{
+							aux.Insert(95,"      "+pack.stock_pack+".00");
+						}
+						else if (pack.stock_pack.ToString().Length == 2)
+						{
+							aux.Insert(95,"     "+pack.stock_pack+".00");
+						}
+						else if (pack.stock_pack.ToString().Length == 3)
+						{
+							aux.Insert(95,"    "+pack.stock_pack+".00");
+						}
+						else if (pack.stock_pack.ToString().Length == 4)
+						{
+							aux.Insert(95,"   "+pack.stock_pack+".00");
+						}
+						writer.WriteLine(aux);
+						logger.logData(aux.ToString());
+						aux.Clear();
+					}
+
+				}
+				if (!esPack)
+				{
+					writer.WriteLine(texto);
+				}
+				
+				
+			}
+			writer.Close();
+		}
+		
+		//Calcula el stock de cada pack y lo escribe en la lista de packs
+		public void calcularStock()
+		{
+			int stockLamp=0;
+			string auxString="";
+			int aux = 0;
+			Pack packaux;
+			foreach (string linea in listaProductos)
+			{
+				for (int x = 0; x < listaPacks.Count; x++)
+				{
+					if (linea.Contains(listaPacks[x].sku_lamp.ToString()))
+					{
+						aux=95;
+						auxString="";
+						while (linea[aux]==' ')
+						{
+							aux++;
+						}
+						
+						while (linea[aux]!='.')
+						{
+							auxString=auxString+linea[aux];
+							aux++;
+						}
+						
+						
+						
+						stockLamp=Int32.Parse(auxString);
+						
+						packaux = new Pack(listaPacks[x].sku_lamp,listaPacks[x].sku_pack,listaPacks[x].cant_pack);
+						packaux.stock_pack=(int)(Math.Round((decimal)stockLamp/listaPacks[x].cant_pack,0)-RESTA_PACKS);
+						if (packaux.stock_pack<0)
+						{
+							packaux.stock_pack=0;
+						}
+						listaPacks[x]=packaux;
+						
+						
+						
+						
+
+					}
+					
+
+					
+					
+					
+				}
+				
+			}
+		}
+		
+		public void sendErrorEmail()
+		{
+			try
+			{
+				var post = new NameValueCollection();
+				post.Add("devid", "v56C2E1A54CA93CC");
+
+				using (var wc = new WebClient())
+				{
+					wc.UploadValues("http://api.pushingbox.com/pushingbox", post);
+				}
+				
+				post = null;
+			}
+			catch (WebException e)
+			{
+				logger.logData("EXEPCION: "+e);
+			}
+		}
+	}
+}
---
+++ Actualizar_Stock_Packs/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: usuario
+ * Date: 16/01/2015
+ * Time: 12:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Actualizar_Stock_Packs
+{
+	class Program
+	{
+		
+		
+		public static void Main(string[] args)
+		{
+			string rutaArchivoPacks = Settings1.Default.rutaArchivoPacks;;
+			
+			string rutaArchivoStock = Settings1.Default.rutaArchivoStock;
+			
+			EditorTxt editorTxt = new EditorTxt();
+			
+			editorTxt.logger.logData("COMENZANDO LA EJECUCION");
+			try {
+				editorTxt.leerPacks(rutaArchivoPacks);
+				editorTxt.leerArchivoStock(rutaArchivoStock);
+				editorTxt.chequearExistenciaSKU(rutaArchivoStock);
+				editorTxt.calcularStock();
+				editorTxt.grabarStocks(rutaArchivoStock);
+			}
+			catch (Exception e)
+			{
+				editorTxt.logger.logData("ERROR: "+e);
+			}
+			
+			
+			editorTxt.logger.logData("EJECUCION COMPLETADA CON EXITO");
+		}
+		
+		
+	}
+}
